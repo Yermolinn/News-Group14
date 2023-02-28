b7b825afd6be0b49fd424bde1094eb3d2a95da16
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-02-28 17:16:38 +0200

    start files 0.2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import { getNews } from "./components/searchNews/fetchNews";
+import './components/categories/categories.js';
+import './components/favoriteNews/favoriteNews.js';
+import './components/firebaseAPI/firebaseAPI.js';
+import './components/footer/footer.js';
+import './components/loader/loader.js';
+import './components/main/main.js';
+import './components/mode-switcher/mode-switcher.js';
+import './components/newsCard/newsCard.js';
+import './components/pagination/pagination.js';
+import './components/readNews/readNews.js';
+import './components/render/render.js';
+import './components/searchNews/fetchNews.js';
+import './components/weather/weather.js';
+
 
 
 
