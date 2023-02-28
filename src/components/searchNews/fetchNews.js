61419add68faa7d2331ae869bcad4466fc63d303
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-02-28 18:44:07 +0200

    Update fetchNews.js

--- src/components/searchNews/fetchNews.js
+++ src/components/searchNews/fetchNews.js
@@ -2,29 +2,29 @@ const ENDPOINT = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 const API_KEY = "api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ";
 
 export async function getNews(query) {
-const URL = `${ENDPOINT}?q=${query}&${API_KEY}`;
+  const URL = `${ENDPOINT}?${API_KEY}&q=${query}`;
 
   return await fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
+    .then(data => data.json())
+    .then(({response}) => {
+      console.log(response);
+
+      response.docs.map(article => {
+        console.log(article);
+      })
 })
         
 }
-
-
-
-
 
 const formEl = document.getElementById('search-form');
 
 formEl.addEventListener('submit', onSubmit);
 
-
 function onSubmit(e) {
   e.preventDefault();
-    const formEl = e.currentTarget;
-    const value = formEl.elements.searchQuery.value.trim();
+  const formEl = e.currentTarget;
+  const value = formEl.elements.searchQuery.value.trim();
+  console.log(value);
   getNews.query = value;
   getNews().finally(() => formEl.reset());
 }
