89097a945411c433cffd31e06fce06ce0b0f9753
Maksym Kikot <maxym1111111@gmail.com>
2023-03-02 01:41:51 +0200

    first push

--- src/components/ArticlesSearchAPI/ArticlesSearchAPI.js
+++ src/components/ArticlesSearchAPI/ArticlesSearchAPI.js
@@ -1,12 +1,12 @@
-const ENDPOINT = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-const API_KEY = "api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ";
+const ENDPOINT = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+const API_KEY = 'api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ';
 
 export async function getNews(query) {
   const URL = `${ENDPOINT}?${API_KEY}&q=${query}`;
 
   return await fetch(URL)
     .then(data => data.json())
-    .then((response) => console.log(response.response.docs))
+    .then(response => console.log(response.response.docs));
 }
 
 const formEl = document.getElementById('search-form');
--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,38 +1,77 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const refs = {
-  arrowUp: document.querySelector('.icon-arrow-up'),
-  arrowDown: document.querySelector('.icon-arrow-down'),
-};
-// import('flatpickr/dist/themes/dark.css');
+import { getNews } from '../ArticlesSearchAPI/ArticlesSearchAPI';
 
-let selectedDate = flatpickr('#date-picker', {
+const refs = {
+  arrowUpEL: document.querySelector('.icon-arrow-up'),
+  arrowDownEL: document.querySelector('.icon-arrow-down'),
+  iconDateEl: document.querySelector('.icon-date'),
+  inputFlPickr: document.querySelector('.flatpickr'),
+};
+
+// import('flatpickr/dist/themes / dark.css');
+
+const options = {
   maxDate: 'today',
-  defaultDate: 'today',
+  // defaultDate: 'today',
   dateFormat: 'd/m/Y',
   onOpen: [
     function () {
-      refs.arrowDown.classList.remove('hide');
-      refs.arrowUp.classList.add('icons-active');
+      refs.arrowUpEL.classList.remove('hide');
+      refs.arrowDownEL.classList.add('hide');
+      refs.iconDateEl.classList.add('icon-active');
+      refs.inputFlPickr.classList.add('placeholder-white');
     },
   ],
-});
+  onClose: [
+    function () {
+      refs.arrowUpEL.classList.add('hide');
+      refs.arrowDownEL.classList.remove('hide');
+      refs.iconDateEl.classList.remove('icon-active');
+      refs.inputFlPickr.classList.remove('placeholder-white');
+    },
+  ],
+  onChange: [
+    async function (time, second, third) {
+      console.log(time);
+      console.log(second);
+      console.log(third);
 
+      // console.log(data);
+      // const newArr = data.map(elem => {
+      //   console.log(elem);
+      // });
+    },
+  ],
+};
+
+let selectedDate = flatpickr('#date-picker', options);
+
+function qwe() {
+  const data = getNews();
+  data.then(arr => {
+    arr.map(elem => {
+      console.log(elem.pub_date);
+    });
+  });
+}
+
+qwe();
 {
   /* <div class="input-date">
-  <svg class="icon-date" width="19" height="19">
-    <use href="./images/sprite.svg#icon-date"></use>
-  </svg>
+    <svg class="icon-date" width="19" height="19">
+        <use href="./images/sprite.svg#icon-date"></use>
+    </svg>
 
-  <input type="text" id="date-picker" class="flatpickr">
-  
-  <svg class="icon-arrow-down" width="9" height="9.14">
-    <use href="./images/sprite.svg#icon-arrow-down"></use>
-  </svg>
+    <input type="text" id="date-picker" class="flatpickr" placeholder="Виберіть дату...">
 
-  <svg class="icon-arrow-up hide" width="9" height="9.14">
-    <use href="./images/sprite.svg#icon-arrow-up"></use>
-  </svg>
+    <svg class="icon-arrow-down" width="9" height="9.14">
+        <use href="./images/sprite.svg#icon-arrow-down"></use>
+    </svg>
+
+    <svg class="icon-arrow-up hide" width="9" height="9.14">
+        <use href="./images/sprite.svg#icon-arrow-up"></use>
+    </svg>
 </div> */
 }
