f2f525b9d71a7061c428c72288ff5734d83f222c
TinaBlshvl <pro.web.design.tina@gmail.com>
2023-03-03 00:36:00 +0000

    Search update

--- src/components/ArticlesSearchAPI/ArticlesSearchAPI.js
+++ src/components/ArticlesSearchAPI/ArticlesSearchAPI.js
@@ -1,15 +1,18 @@
-const ENDPOINT = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-const API_KEY = 'api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ';
+import Notiflix from "notiflix";
+
+const ENDPOINT = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+const API_KEY = "api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ";
 
 export async function getNews(query) {
   const URL = `${ENDPOINT}?${API_KEY}&q=${query}`;
 
   return await fetch(URL)
     .then(data => data.json())
-    .then(response => console.log(response.response.docs));
+    .then((response) => console.log(response.response.docs))
 }
 
 const formEl = document.getElementById('search-form');
+const newsList = document.getElementById('newsList')
 
 formEl.addEventListener('submit', onSubmit);
 
@@ -17,6 +20,25 @@ function onSubmit(e) {
   e.preventDefault();
   const formEl = e.currentTarget;
   const value = formEl.elements.searchQuery.value.trim();
-  getNews(value);
-  formEl.reset();
+
+  getNews(value).then(({ articles }) => {
+    if (articles.length === 0) throw new Error("No data");
+    return articles.reduce((markup, article) =>
+      createMarkup(article) + markup, "");
+  })
+    .then(updateCard)
+    .catch(onError)
+    .finally(() => formEl.reset());
+}
+
+function updateCard(markup) {
+  newsList.innerHTML = markup
+}
+
+function createMarkup() {
+
+}
+
+function onError() {
+  Notiflix.Notify.failure('Oops, there is no article with that name!');
 }
