f934ae557ded5a5da972cf80551bd78fe0886ca7
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-03-07 23:30:17 +0200

    Update ArticlesSearchAPI.js

--- src/components/ArticlesSearchAPI/ArticlesSearchAPI.js
+++ src/components/ArticlesSearchAPI/ArticlesSearchAPI.js
@@ -5,7 +5,7 @@ const axios = require('axios').default;
 const ENDPOINT = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 const API_KEY = 'api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ';
 
-import { default as axios } from 'axios';
+// import { default as axios } from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
