22b9e35514619c56a4dd4b582355f1e25506fadd
RudenkoOV <kpaxx@ukr.net>
2023-03-09 18:49:56 +0200

    vers4

--- src/read.js
+++ src/read.js
@@ -1,3 +1,4 @@
+import './components/readNews/readNews.js';
 import './components/header/input-hover';
 import './components/header/open-menu';
 import './components/header/close-menu';
@@ -8,11 +9,11 @@ import './components/timesNewswire/timesNewswire';
 // import './components/mostPopular/mostPopular';
 // import './components/render/render.js';
 import './components/button/button.js';
-import './components/favoriteNews/favoriteNews.js';
+// import './components/favoriteNews/favoriteNews.js';
 import './components/footer/footer.js';
 import './components/preLoader/preLoader.js';
 import './components/main/main.js';
-// import './components/mode-switcher/mode-switcher.js';
-// import './components/newsCard/newsCard.js';
+import './components/modeSwitcher/modeSwitcher.js';
+import './components/newsCard/newsCard.js';
 import './components/pagination/pagination.js';
-import './components/readNews/readNews.js';
+
