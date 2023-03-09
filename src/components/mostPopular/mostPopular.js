0969de64f3ab17a4a8e042fb7bf28bfb1036cf90
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-03-10 01:48:47 +0200

    Update mostPopular.js

--- src/components/mostPopular/mostPopular.js
+++ src/components/mostPopular/mostPopular.js
@@ -1,117 +1,117 @@
-const newsList = document.querySelector('.news-list');
-const API_KEY = 'api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ';
-import localStorageService from '../localStorageService/localStorageService';
-import {
-  checkLokalStorage,
-  removeFavoriteBtnHTML,
-  addFavoriteBtnHTML,
-  alreadyRead,
-  handleFavorite,
-  handleRead,
-} from '../render/render';
-const axios = require('axios').default;
-class MostPopularApiService {
-  //   constructor() {
-  //     this.page = 1;
-  //     this.searchQuery = '';
-  //     this.perPage = 40;
-  //   }
+// const newsList = document.querySelector('.news-list');
+// const API_KEY = 'api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ';
+// import localStorageService from '../localStorageService/localStorageService';
+// import {
+//   checkLokalStorage,
+//   removeFavoriteBtnHTML,
+//   addFavoriteBtnHTML,
+//   alreadyRead,
+//   handleFavorite,
+//   handleRead,
+// } from '../render/render';
+// const axios = require('axios').default;
+// class MostPopularApiService {
+//   //   constructor() {
+//   //     this.page = 1;
+//   //     this.searchQuery = '';
+//   //     this.perPage = 40;
+//   //   }
 
-  async getNews() {
-    // const URL = `${ENDPOINT}?${API_KEY}&q=${this.searchQuery}`; // це для пошуку
-    const mostPopularUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?${API_KEY}`;
-    const response = await axios.get(mostPopularUrl);
-    // console.log(response.data.results[0].media[0]['media-metadata'][0].url);
-    return response.data.results;
-  }
+//   async getNews() {
+//     // const URL = `${ENDPOINT}?${API_KEY}&q=${this.searchQuery}`; // це для пошуку
+//     const mostPopularUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?${API_KEY}`;
+//     const response = await axios.get(mostPopularUrl);
+//     // console.log(response.data.results[0].media[0]['media-metadata'][0].url);
+//     return response.data.results;
+//   }
 
-  //   nextPage() {
-  //     this.page += 1;
-  //   }
-  //   resetPage() {
-  //     this.page = 1;
-  //   }
-}
-async function render() {
-  //рендерить  популярні новини
-  const mostPopularApiService = new MostPopularApiService();
+//   //   nextPage() {
+//   //     this.page += 1;
+//   //   }
+//   //   resetPage() {
+//   //     this.page = 1;
+//   //   }
+// }
+// async function render() {
+//   //рендерить  популярні новини
+//   const mostPopularApiService = new MostPopularApiService();
 
-  const articles = await mostPopularApiService.getNews();
-  console.log('🚀 ~ articles', articles);
-  if (articles.length === 0) throw new Error('No data');
-  let i = 0;
-  const card = articles.reduce((markup, article) => {
-    i++;
-    return markup + createMostPopularNews(article, i);
-  }, '');
+//   const articles = await mostPopularApiService.getNews();
+//   // console.log('🚀 ~ articles', articles);
+//   if (articles.length === 0) throw new Error('No data');
+//   let i = 0;
+//   const card = articles.reduce((markup, article) => {
+//     i++;
+//     return markup + createMostPopularNews(article, i);
+//   }, '');
 
-  updateCard(card);
-}
+//   updateCard(card);
+// }
 
-function updateCard(markup) {
-  newsList.innerHTML = markup;
-}
+// function updateCard(markup) {
+//   newsList.innerHTML = markup;
+// }
 
-function createMostPopularNews(article, i) {
-  // створює розмітку популярних новин
-  const { abstract, published_date, section, title, media, url, id } = article;
-  setTimeout(() => {
-    // виконається після того як з'являться картки
-    const btn = document.querySelector(`.favorite-btn--${id}`);
-    const link = document.querySelector(`.news-link--${id}`);
-    const p = document.querySelector(`.isread--${id}`);
-    const card = document.querySelector(`.news-card--${id}`);
+// function createMostPopularNews(article, i) {
+//   // створює розмітку популярних новин
+//   const { abstract, published_date, section, title, media, url, id } = article;
+//   setTimeout(() => {
+//     // виконається після того як з'являться картки
+//     const btn = document.querySelector(`.favorite-btn--${id}`);
+//     const link = document.querySelector(`.news-link--${id}`);
+//     const p = document.querySelector(`.isread--${id}`);
+//     const card = document.querySelector(`.news-card--${id}`);
 
-    let isFav = false;
-    let localFavorite = localStorageService.load('favorite');
-    console.log(localFavorite);
-    let checkFavorite = checkLokalStorage(article, localFavorite);
-    if (checkFavorite === true) {
-      btn.innerHTML = removeFavoriteBtnHTML;
-      btn.classList.add('favorite-btn--active');
-    }
-    let localArr = localStorageService.load('readMoreLocal');
-    let checkRead = checkLokalStorage(article, localArr);
-    if (checkRead === true) {
-      p.innerHTML = alreadyRead;
-      card.classList.add('opacity');
-    }
-    btn.onclick = handleFavorite(isFav, article, btn);
-    link.onclick = handleRead(article, p, card);
-  }, 0);
-  if (media.length !== 0) {
-    defaultImg = media[0]['media-metadata'][2].url;
-  }
-  return `<div class="news-card ${`news-card--${id}`} grid grid-item-${i}">
+//     let isFav = false;
+//     let localFavorite = localStorageService.load('favorite');
+//     // console.log(localFavorite);
+//     let checkFavorite = checkLokalStorage(article, localFavorite);
+//     if (checkFavorite === true) {
+//       btn.innerHTML = removeFavoriteBtnHTML;
+//       btn.classList.add('favorite-btn--active');
+//     }
+//     let localArr = localStorageService.load('readMoreLocal');
+//     let checkRead = checkLokalStorage(article, localArr);
+//     if (checkRead === true) {
+//       p.innerHTML = alreadyRead;
+//       card.classList.add('opacity');
+//     }
+//     btn.onclick = handleFavorite(isFav, article, btn);
+//     link.onclick = handleRead(article, p, card);
+//   }, 0);
+//   if (media.length !== 0) {
+//     defaultImg = media[0]['media-metadata'][2].url;
+//   }
+//   return `<div class="news-card ${`news-card--${id}`} grid grid-item-${i}">
 
-    <div class="top-wrap">
-      <img
-        src="${defaultImg}"
-        loading="lazy"
-        width="288"
-        height="395"
-        class="news-img"
-      />
-      <p class="isread ${`isread--${id}`}"></p>
-      <div class="category-wrap">
-        <p class="top-text">${section}</p>
-      </div>
-      <button class="favorite-btn ${`favorite-btn--${id}`}" data-id="${id}">
-        ${addFavoriteBtnHTML}
-      </button>
-    </div>
-    <div class="info">
-      <h2 class="info-item">${title}</h2>
-      <p class="describe">${abstract.slice(0, 150) + '...'}</p>
-      <div class="lower-content">
-        <p class="news-date">${published_date
-          .slice(0, 10)
-          .replaceAll('-', '/')}</p>
-        <a class="news-link ${`news-link--${id}`} link" href="${url}"  onclick="handleRead()" target="_blank">Read more</a>
-      </div>
-    </div>
-  </div>
+//     <div class="top-wrap">
+//       <img
+//         src="${defaultImg}"
+//         loading="lazy"
+//         width="288"
+//         height="395"
+//         class="news-img"
+//       />
+//       <p class="isread ${`isread--${id}`}"></p>
+//       <div class="category-wrap">
+//         <p class="top-text">${section}</p>
+//       </div>
+//       <button class="favorite-btn ${`favorite-btn--${id}`}" data-id="${id}">
+//         ${addFavoriteBtnHTML}
+//       </button>
+//     </div>
+//     <div class="info">
+//       <h2 class="info-item">${title}</h2>
+//       <p class="describe">${abstract.slice(0, 150) + '...'}</p>
+//       <div class="lower-content">
+//         <p class="news-date">${published_date
+//           .slice(0, 10)
+//           .replaceAll('-', '/')}</p>
+//         <a class="news-link ${`news-link--${id}`} link" href="${url}"  onclick="handleRead()" target="_blank">Read more</a>
+//       </div>
+//     </div>
+//   </div>
   
-`;
-}
-render();
+// `;
+// }
+// render();
