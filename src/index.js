844a536b4b5866f82cae68c95044e34b701ccf11
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-03-10 00:52:14 +0200

    update 0.94

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import './components/header/open-menu';
 import './components/header/close-menu';
 import './components/header/current-page';
 import './components/header/menu-current-page';
+import './components/modeSwitcher/modeSwitcher.js';
 import './components/render/render.js';
 import './components/articlesSearch/articlesSearch.js';
-import './components/timesNewswire/timesNewswire';
 import './components/mostPopular/mostPopular';
 import './components/calendar/calendar.js';
-import './components/button/button.js';
 import './components/categories/categories.js';
-import './components/footer/footer.js';
-import './components/preLoader/preLoader.js';
-import './components/main/main.js';
-import './components/modeSwitcher/modeSwitcher.js';
 import './components/newsCard/newsCard.js';
+import './components/localStorageService/localStorageService.js'
 import './components/pagination/pagination.js';
 // import './components/readNews/readNews.js';
 import './components/openWeather/openWeather.js';
+import './components/preLoader/preLoader.js';
+import './components/footer/footer.js';
+
+
