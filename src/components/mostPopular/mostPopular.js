e70f87b14bcbe6d6158c2103728cc43a6dc7abe4
Bogdan Nazarchuk <111483751+Virtuozio@users.noreply.github.com>
2023-03-10 14:24:13 +0200

    changes

--- src/components/mostPopular/mostPopular.js
+++ src/components/mostPopular/mostPopular.js
@@ -2,14 +2,15 @@ const newsList = document.querySelector('.news-list');
 const API_KEY = 'api-key=HR9YxGV98GGTmMcKHA5eY4Aer5nJgRvJ';
 import localStorageService from '../localStorageService/localStorageService';
 import {
-  checkLokalStorage,
   removeFavoriteBtnHTML,
   addFavoriteBtnHTML,
   alreadyRead,
   handleFavorite,
   handleRead,
+  checkLokalStorage,
 } from '../render/render';
 const axios = require('axios').default;
+
 class MostPopularApiService {
   //   constructor() {
   //     this.page = 1;
@@ -19,7 +20,7 @@ class MostPopularApiService {
 
   async getNews() {
     // const URL = `${ENDPOINT}?${API_KEY}&q=${this.searchQuery}`; // це для пошуку
-    const mostPopularUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?${API_KEY}`;
+    const mostPopularUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?${API_KEY}`;
     const response = await axios.get(mostPopularUrl);
     // console.log(response.data.results[0].media[0]['media-metadata'][0].url);
     return response.data.results;
@@ -42,6 +43,20 @@ async function render() {
   let i = 0;
   const card = articles.reduce((markup, article) => {
     i++;
+    let image = `https://cdn.create.vista.com/api/media/small/251043028/stock-photo-selective-focus-black-news-lettering`;
+    // console.log(defaultImg);
+    if (article.media.length !== 0) {
+      image = article.media[0]['media-metadata'][2].url;
+    }
+    article = {
+      image: image,
+      section: article.section,
+      title: article.title,
+      description: article.abstract,
+      date: article.published_date,
+      url: article.url,
+      id: article.id,
+    };
     return markup + createMostPopularNews(article, i);
   }, '');
 
@@ -52,10 +67,9 @@ function updateCard(markup) {
   newsList.innerHTML = markup;
 }
 
-
 function createMostPopularNews(article, i) {
   // створює розмітку популярних новин
-  const { abstract, published_date, section, title, media, url, id } = article;
+  const { image, section, title, description, date, url, id } = article;
   setTimeout(() => {
     // виконається після того як з'являться картки
     const btn = document.querySelector(`.favorite-btn--${id}`);
@@ -65,7 +79,6 @@ function createMostPopularNews(article, i) {
 
     let isFav = false;
     let localFavorite = localStorageService.load('favorite');
-    // console.log(localFavorite);
     let checkFavorite = checkLokalStorage(article, localFavorite);
     if (checkFavorite === true) {
       btn.innerHTML = removeFavoriteBtnHTML;
@@ -80,15 +93,11 @@ function createMostPopularNews(article, i) {
     btn.onclick = handleFavorite(isFav, article, btn);
     link.onclick = handleRead(article, p, card);
   }, 0);
-  let defaultImg = `https://cdn.create.vista.com/api/media/small/251043028/stock-photo-selective-focus-black-news-lettering`;
-  if (media.length !== 0) {
-    defaultImg = media[0]['media-metadata'][2].url;
-  }
   return `<div class="news-card ${`news-card--${id}`} grid grid-item-${i}">
 
     <div class="top-wrap">
       <img
-        src="${defaultImg}"
+        src="${image}"
         loading="lazy"
         width="288"
         height="395"
@@ -104,11 +113,9 @@ function createMostPopularNews(article, i) {
     </div>
     <div class="info">
       <h2 class="info-item">${title}</h2>
-      <p class="describe">${abstract.slice(0, 150) + '...'}</p>
+      <p class="describe">${description.slice(0, 150) + '...'}</p>
       <div class="lower-content">
-        <p class="news-date">${published_date
-          .slice(0, 10)
-          .replaceAll('-', '/')}</p>
+        <p class="news-date">${date.slice(0, 10).replaceAll('-', '/')}</p>
         <a class="news-link ${`news-link--${id}`} link" href="${url}"  onclick="handleRead()" target="_blank">Read more</a>
       </div>
     </div>
